4b15824dd0df25e86d80348dc4756103fac946a6
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-06-23 04:35:14 +0530

    Built HomeScreen

---
+++ lib/screens/home/components/genres.dart
@@ -0,0 +1,28 @@
+import 'package:cinema_roll/components/genre_card.dart';
+import 'package:cinema_roll/constants.dart';
+import 'package:flutter/material.dart';
+
+class Genres extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<String> genres = [
+      "Action",
+      "Crime",
+      "Comedy",
+      "Drama",
+      "Horror",
+      "Animation"
+    ];
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      height: 36,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: genres.length,
+        itemBuilder: (context, index) => GenreCard(
+          genre: genres[index],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/components/movie_card.dart
@@ -0,0 +1,69 @@
+import 'package:cinema_roll/constants.dart';
+import 'package:cinema_roll/models/movie.dart';
+import 'package:cinema_roll/screens/details/detail_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie movie;
+
+  const MovieCard({Key key, this.movie}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: InkWell(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailsScreen(
+              movie: movie,
+            ),
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [kDefaultShadow],
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(movie.poster),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+              child: Text(
+                movie.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5
+                    .copyWith(fontWeight: FontWeight.w600),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/star_fill.svg",
+                  height: 20,
+                ),
+                SizedBox(
+                  width: kDefaultPadding / 2,
+                ),
+                Text(
+                  "${movie.rating}",
+                  style: Theme.of(context).textTheme.bodyText2,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
