cc4a8738f10ed75141707ad2063d85fb0c6bd4c5
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-06-23 04:36:43 +0530

    Tweaked Animations

---
+++ lib/screens/details/components/title_duration_add.dart
@@ -0,0 +1,69 @@
+import 'package:cinema_roll/constants.dart';
+import 'package:cinema_roll/models/movie.dart';
+import 'package:flutter/material.dart';
+
+class TitleDurationAdd extends StatelessWidget {
+  const TitleDurationAdd({
+    Key key,
+    @required this.movie,
+  }) : super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  movie.title,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                SizedBox(height: kDefaultPadding / 2),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "${movie.year}",
+                      style: TextStyle(color: kTextLightColor),
+                    ),
+                    SizedBox(width: kDefaultPadding),
+                    Text(
+                      "PG-13",
+                      style: TextStyle(color: kTextLightColor),
+                    ),
+                    SizedBox(width: kDefaultPadding),
+                    Text(
+                      "2h 32min",
+                      style: TextStyle(color: kTextLightColor),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 64,
+            width: 64,
+            child: FlatButton(
+              onPressed: () {},
+              color: kSecondaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Icon(
+                Icons.add,
+                size: 28,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
