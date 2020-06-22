fa805a9c46c10b11fc6922aec8ed386c930db14e
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-06-23 04:33:42 +0530

    Set up skeleton

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cinema_roll/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cinema Roll',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
