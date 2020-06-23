ae4f7f975e8e0f2220452c09d7f9019cf0da1781
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-06-23 13:58:22 +0530

    Added Animation

--- lib/screens/home/components/movie_card.dart
+++ lib/screens/home/components/movie_card.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:cinema_roll/constants.dart';
 import 'package:cinema_roll/models/movie.dart';
 import 'package:cinema_roll/screens/details/detail_screen.dart';
@@ -12,58 +13,57 @@ class MovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: InkWell(
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => DetailsScreen(
-              movie: movie,
+      child: OpenContainer(
+        closedBuilder: (context, action) => buildMovieCard(context),
+        openElevation: 0,
+        closedElevation: 0,
+        openBuilder: (context, action) => DetailsScreen(movie: movie),
+      ),
+    );
+  }
+
+  Column buildMovieCard(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              boxShadow: [kDefaultShadow],
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(movie.poster),
+              ),
             ),
           ),
         ),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  boxShadow: [kDefaultShadow],
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(movie.poster),
-                  ),
-                ),
-              ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+          child: Text(
+            movie.title,
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.w600),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/icons/star_fill.svg",
+              height: 20,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-              child: Text(
-                movie.title,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline5
-                    .copyWith(fontWeight: FontWeight.w600),
-              ),
+            SizedBox(
+              width: kDefaultPadding / 2,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/icons/star_fill.svg",
-                  height: 20,
-                ),
-                SizedBox(
-                  width: kDefaultPadding / 2,
-                ),
-                Text(
-                  "${movie.rating}",
-                  style: Theme.of(context).textTheme.bodyText2,
-                )
-              ],
+            Text(
+              "${movie.rating}",
+              style: Theme.of(context).textTheme.bodyText2,
             )
           ],
-        ),
-      ),
+        )
+      ],
     );
   }
 }
